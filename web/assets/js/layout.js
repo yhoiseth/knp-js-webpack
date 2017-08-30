d6682735051afbe6c75a0c3c34705f630778e11a
Yngve Høiseth <yngve@hoiseth.net>
2017-08-30 21:18:24 +0200

    Use Webpack jQuery plugin

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const $ = require('jquery');
-window.jQuery = $;
+
 require('bootstrap');
 
 $(document).ready(function() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
@@ -9,5 +10,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'web','build'),
     filename: '[name].js',
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      jQuery: 'jquery',
+      $: 'jquery'
+    }),
+  ],
 };
